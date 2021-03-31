6b2e027a1a22fc4b2bfcd06c49b52536fff2f429
jstepcom <j.stepcom@gmail.com>
2021-03-30 20:36:22 -0600

    improve axios and create data object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Weather from './Weather';
 import SearchEngine from './SearchEngine';
-import Velociraptor from './img/Velociraptor.gif';
+
 import './style/App.css';
 
 
@@ -9,8 +9,6 @@ function App() {
     <div className="App">
       <div className="container-fluid">
         <Weather />
-        <img src={Velociraptor} className="App-velociraptor" alt="velociraptor" /> 
-        <SearchEngine />
         <footer>This code is <a href="https://github.com/jstepcom/weather-react" target="_blank">Open Sourced</a> on github</footer>
       </div>
     </div>
