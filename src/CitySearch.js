ad13e549b904095472ab468f006bddc342290feb
jstepcom <j.stepcom@gmail.com>
2021-04-11 15:33:54 -0500

    add geoloc elements

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCrosshairs } from '@fortawesome/free-solid-svg-icons'
 import SweetAlert2 from 'react-sweetalert2';
 
-
 export default function CitySearch({defaultCity}){
   const currentloc = <FontAwesomeIcon icon={faCrosshairs} />
   let [element, setElement] = useState();
@@ -28,6 +27,13 @@ export default function CitySearch({defaultCity}){
   function handleCity(event){
     setCity(event.target.value); 
   }
+  function getCity(possition){
+    console.log(possition)
+
+  }
+  function handleDefault(){
+    navigator.geolocation.getCurrentPosition(getCity)
+  }
 
   function handleSubmit(event){
     event.preventDefault();
@@ -45,8 +51,9 @@ export default function CitySearch({defaultCity}){
           <img src={logo} alt="react-logo"  className="Weather-logo d-inline-block align-text-top"/>
           <h1> Weather app </h1>
           <small>by JSCO</small>
-          <button>{currentloc}</button>
+          
           <form onSubmit = {handleSubmit} requiredvalue={city} className="d-flex Weather-form">
+            <button className="curr-loc" onClick={handleDefault}>{currentloc}</button>
             <input type="search" placeholder="Search City" autoFocus= "on" className="form-control me-2" onChange={handleCity}/>
             <button className="btn" type="submit">Search</button>
           </form> 
